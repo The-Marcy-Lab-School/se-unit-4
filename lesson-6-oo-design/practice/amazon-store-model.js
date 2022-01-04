6fb577be397fb12789f3007b9fda7413c2c3c670
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2022-01-04 12:42:11 -0500

    Update amazon-store-model.js

--- lesson-6-oo-design/practice/amazon-store-model.js
+++ lesson-6-oo-design/practice/amazon-store-model.js
@@ -78,9 +78,9 @@ class User{
 
 let markers = new Item('Markers', 9.99, 50, 'Best markers in the world')
 let peter = new User('peter', 'peter@gmail.com', 'Credit Card', true)
-peter.addItem(markers, 5)   // What does this print out?
-peter.cart                  // What does this print out?
-peter.cart.numOfItems()     // What does this print out?
-peter.cart.getTotal()       // What does this print out?
+console.log(peter.addItem(markers, 5))   // What does this print out?
+console.log(peter.cart)                  // What does this print out?
+console.log(peter.cart.numOfItems())     // What does this print out?
+console.log(peter.cart.getTotal())       // What does this print out?
 markers.addReview(4, "nice markers, but not the best in the world")
-markers.reviews             // What does this print out?
+console.log(markers.reviews)             // What does this print out?
